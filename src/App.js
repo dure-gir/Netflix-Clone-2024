9b57ce84485426c556720572ee7416ea067d77bf
Dura1617 <duragirma@gmail.com>
2024-08-23 12:30:00 +0300

    first commit Header and footer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Home from './pages/Home/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>estifanos</h1>
+   
+    <Home />
+    </div>
+  );
+}
+
+export default App;
